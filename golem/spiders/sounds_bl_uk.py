cf37c395710743af1b0b3b749881ea6b356ab036
Andrew N. Jackson <anj@anjackson.net>
2022-10-24 22:37:39 +0100

    Essentially functional sounds.bl.uk crawler, not fully integrated. #17.

--- golem/spiders/sounds_bl_uk.py
+++ golem/spiders/sounds_bl_uk.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 import time
 import scrapy
-#from scrapy_headless import HeadlessRequest
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
+from pathlib import Path
+from scrapy_playwright.page import PageMethod
 
 class SoundsBlUkSpider(scrapy.Spider):
     name = 'sounds.bl.uk'
@@ -19,66 +17,44 @@ class SoundsBlUkSpider(scrapy.Spider):
         #'http://sounds.bl.uk/Sound-recording-history/Equipment'
     ]
 
-    def start_requests(self):
-        pass
-        #for url in self.start_urls:
-        #    yield HeadlessRequest(url=url, 
-        #        callback=self.parse,
-        #        driver_callback=self.run_macros)
+    
 
-    def parse(self, response):
-        print("RESPONSE")
-        print(len(response.css('a::attr(href)')))
+    def start_requests(self):
+        # Load userscript
+        path = Path(__file__).parent / "..//userscripts/sounds-bl.uk.user.js"
+        with path.open() as f:
+            userscript = f.read()
+        # Launch
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url=url,
+                meta=dict(
+                    playwright=True,
+                    playwright_context=1,
+                    playwright_page_methods=[
+                        PageMethod("evaluate", userscript),
+                    ],
+                )
+            )
+
+    async def parse(self, response):
+        self.logger.info(f"RESPONSE has {len(response.css('a::attr(href)'))} links.")
         for href in response.css('a::attr(href)'):
-            print(response.urljoin(href.extract()))
+            self.logger.info(f"LINK: {response.urljoin(href.extract())}")
             #yield response.follow(href, self.parse)
 
-    closed_list_css ='div[aria-hidden="false"] li[class="closed"] a'
-
-    def get_closed_count(self, driver):
-        return len(driver.find_elements(By.CSS_SELECTOR, self.closed_list_css))
-    
-    def open_all_visible_lists(self, driver):
-        print("LISTS")
-        to_open = self.get_closed_count(driver)
-        while to_open > 0:
-            # Click on a closed list item:
-            list_el = driver.find_element(By.CSS_SELECTOR, self.closed_list_css)
-            list_el.click()
-            # Wait for the click to be implemented:
-            time.sleep(0.5)
-            # Re-count items waiting to be opened:
-            to_open = self.get_closed_count(driver)
-            print("There are %i closed list items to open." % to_open)
-
-    def run_macros(self, driver):
-        print("MACROS")
-        # Make any players play:
-        #
-        # Unfortunately, this site uses Flash players via JavaScript via SWFObject
-        # view-source:https://sounds.bl.uk/scripts/main.js
-        # This makes 206 partial requests, and so warcprox doens't get the whole file.
-        # It may be possible to interfere via a stacked mitmproxy that turns to 206 into a 200?
-        # OR just grab all the URLs and download them separately.
-        # A double-crawl like that would be clumsy but would work.
-        # OR the WARCs could be post-processed, stripping out the 206s and
-        # recording all URLs that had 206's and just getting those separately.
-        for playable in driver.find_elements(By.CSS_SELECTOR, ".playable"):
-            print("PLAY %s" % playable)
-            playable.click()
-            # Give it some time to get going...
-            time.sleep(2)
-        # Attempt to flip through all tabs:
-        tab_num = 1
-        while tab_num < 7:
-            tab_selector = "a#ui-id-%i" % tab_num
-            print("TAB %s" % tab_selector)
-            # Check it exists:
-            if driver.find_elements(By.CSS_SELECTOR, tab_selector):
-                driver.find_element(By.CSS_SELECTOR, tab_selector).click();
-                # Give it time to flip:
-                time.sleep(2)
-                # Now open any lists this revealed:
-                self.open_all_visible_lists(driver)
-            tab_num += 1
-        print("DONE MACROS")
+    custom_settings = {
+        "DOWNLOAD_HANDLERS": {
+            "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+            "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+        },
+        "TWISTED_REACTOR": "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
+        "PLAYWRIGHT_CONTEXTS": {
+            1: {
+                "ignore_https_errors": True,
+                "proxy": {
+                    "server": "http://localhost:8080",
+                },
+            },
+        }
+    }
